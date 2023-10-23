dd134dcc2e787383dc31c760bb48c94e57b1f9b6
David Ferere <davidferere@gmail.com>
2023-10-22 20:00:17 -0400

    Update app.js

--- Practica de integracion/src/app.js
+++ Practica de integracion/src/app.js
@@ -75,10 +75,7 @@ app.use('/mongo/carts', cartsRouterMongo);
 app.use('/ecommerce/home', sessionRouter); //Ruta de sesiones y login
 app.use('/ecommerce/user', userRouter); //Ruta de usarios
 
-//app.use('/products', productsRouterMongo); //view de products
-
 app.use('/api', cartsRouterMongo);
-
 
 app.use('/api/sessions', sessionRouter);
 
@@ -92,7 +89,6 @@ app.use('/mongo/chat', chatRouterMongo); //endpoint del chat
 socketServer.on('connection', async socket => {
   console.log("Cliente nuevo conectado")
   const allproducts = await productosMongo.getallProducts();
-  //console.log(response);
   socketServer.emit('prod', allproducts);
   socket.on('message', data => {
     console.log(data);
@@ -100,18 +96,13 @@ socketServer.on('connection', async socket => {
 
   //Enviar catalogo de productos por handlebars
   const query = {};
-  //const allPageProducts = await productosMongo.getpageProducts(query, 1, 10, 1);
   //Traemos productos paginados
-  const allPageProducts = await productosMongo.getallProducts();
-  //console.log("AllPageProducts"); 
-  //console.log(allPageProducts); 
+  const allPageProducts = await productosMongo.getallProducts(); 
   socketServer.emit('product', allPageProducts);
 
   //Traemos lista de productos de carritos con populate
-  //const cid = cartsRouterMongo.returncid; 
   const cid = "650f8a995f9deb7531fb7380"; 
   const getCart = await carritoMongo.getCartProducts(cid);
-  //JSON.stringify(getCart), 
   console.log(getCart);
   console.log(typeof(getCart)); 
   socketServer.emit('cart', getCart);
@@ -131,8 +122,6 @@ socketServer.on('connection', async socket => {
   socket.on('sendNewProduct', async id => {
     console.log(id);
     await productsModel.deleteOne(id);
-    //const allproducts = await productos.getProducts(); 
-    //console.log(response);
     socketServer.emit('prod',);
 
   })
@@ -158,27 +147,18 @@ socketServer.on('connection', async socket => {
 
   socket.on('addproductCarrito', async idprod => {
 
-    //console.log(idprod);
     const idcarrito = !idprod.idcarrito ? null : idprod.idcarrito;
     const idproduct = !idprod.idproduct ? null : idprod.idproduct;
-
-    //console.log(idcarrito);
-    //console.log(idproduct); 
-
-    //const quantity = 1; 
-    //const response = await productos.deleteproductByID(id);
 
     if (idprod == null && idcarrito == null) {
       const response = await carritoMongo.createcart();
     } else {
-      console.log("Vamos agregar un producto al carrito de ALbert");
+      console.log("Vamos agregar un producto al carrito de xxx");
       const response2 = await carritoMongo.addProductCart(idcarrito, idproduct);
-      //console.log(response);
     }
 
     const allproducts = await productosMongo.getallProducts();
     socketServer.emit('prod', allproducts);
-    //socketServer.emit('prod', );
 
   })
 
