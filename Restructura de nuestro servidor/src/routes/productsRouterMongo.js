f37f7c7aa6227f47d3f7f1297589849a239b2a7e
David Ferere <davidferere@gmail.com>
2023-10-30 15:24:55 -0400

    Update productsRouterMongo.js

--- Restructura de nuestro servidor/src/routes/productsRouterMongo.js
+++ Restructura de nuestro servidor/src/routes/productsRouterMongo.js
@@ -4,17 +4,10 @@ import ProductManagerMongo from '../controllers/mongo/productsManagerMongo.js';
 
 const router = Router();
 
-//const pm = require("./ProductManager.js");
-
-//app.use(express.urlencoded({ extended: true }));
-//app.use(express.json());
 
 const productosMongo = new ProductManagerMongo();
 
-/*router.get("/", async (req, res) => {
-    res.render("realTimeProducts", {
-    })
-})*/
+
  /*View de productos catalogo paginaciçion*/
 router.get("/catalog", async (req, res) => {
    
@@ -22,9 +15,6 @@ router.get("/catalog", async (req, res) => {
     console.log("este es req session"); 
     console.log(req.session.email);
     
-    //req.body.username = username;
-
-    //console.log(username); 
 
     res.render("ViewsProducts", { objectsession }); 
 })
@@ -43,30 +33,6 @@ router.get("/current", async (req, res) => {
     res.send(objectsession); 
 })
 
-/*Limite de Productos*/
-/*router.get('/', async (req, res) => { //trae lista de productos con param limit
-
-    var limit = req.query.limit;
-    parseInt(limit);
-    console.log(limit); 
-    console.log("Entre a GET de numero de Productos"); 
-    //const response = await productos.getProducts();
-
-    if (!limit) {
-        const limitconst = 10;
-        const response = await productosMongo.getProducts(limitconst);
-        return res.send(response);
-    } else {
-        const response = await productosMongo.getProducts(limit);
-        return res.send(response);
-    }
-
-    //const slicedArray = response.slice(0, limit);
-
-    //res.send(slicedArray)
-
-    //res.send(response)
-})*/
 
 /*Paginación*/
 router.get('/', async (req, res) => { //trae lista de productos con param limit, page, query y sort
@@ -77,8 +43,6 @@ router.get('/', async (req, res) => { //trae lista de productos con param limit,
     var queryvalue = req.query.queryvalue;
     var sortvalue = req.query.sortvalue; 
 
-    /*res.render("ViewsProducts", {
-    })*/
 
     parseInt(page);
     parseInt(limit);
@@ -87,7 +51,7 @@ router.get('/', async (req, res) => { //trae lista de productos con param limit,
     console.log(query);
     console.log(queryvalue);  
     console.log("Entre a GET de numero de Paginas"); 
-    //const response = await productos.getProducts();
+
 
     if(!sortvalue){
         sortvalue = 1; 
@@ -99,12 +63,12 @@ router.get('/', async (req, res) => { //trae lista de productos con param limit,
         const query = {}; 
         const response = await productosMongo.getpageProducts(query, pageconst, limitconst, sortvalue);
         return response; 
-        //console.log(response); 
+
         return res.send(response); //Habilitar si usa Postman
     } 
 
     if (page && !limit && !query) {
-        //const pageconst = 1;
+ 
         const limitconst = 10; 
         const query = {};
         const response = await productosMongo.getpageProducts(query, page, limitconst, sortvalue);
@@ -112,8 +76,7 @@ router.get('/', async (req, res) => { //trae lista de productos con param limit,
     } 
 
     if (!page && limit && !query) {
-        const pageconst = 1;
-        //const limitconst = 10; 
+        const pageconst = 1; 
         const query = {};
         const response = await productosMongo.getpageProducts(query, pageconst, limit, sortvalue);
         return res.send(response);
@@ -128,9 +91,7 @@ router.get('/', async (req, res) => { //trae lista de productos con param limit,
         return res.send(response);
     } 
 
-    if (page && limit && !query) {
-        //const pageconst = 1;
-        //const limitconst = 10; 
+    if (page && limit && !query) { 
         const query = {};
         const response = await productosMongo.getpageProducts(query, page, limit, sortvalue);
         return res.send(response);
@@ -138,33 +99,26 @@ router.get('/', async (req, res) => { //trae lista de productos con param limit,
 
     if (!page && limit && query) {
         const pageconst = 1;
-        //const limitconst = 10; 
-        //const query = {};
         const queryobject = {category : queryvalue};
         const response = await productosMongo.getpageProducts(queryobject, pageconst, limit, sortvalue);
         return res.send(response);
     }
 
     if (page && !limit && query) {
-        //const pageconst = 1;
+
         const limitconst = 10; 
-        //const query = {};
+
         const queryobject = {category : queryvalue};
         const response = await productosMongo.getpageProducts(queryobject, page, limitconst, sortvalue);
         return res.send(response);
     } else {
-        //const query = query; 
+ 
         const queryobject = {category : queryvalue};
-        //console.log(queryobject);
+
         const response = await productosMongo.getpageProducts(queryobject, page, limit, sortvalue);
         return res.send(response);
     }
 
-    //const slicedArray = response.slice(0, limit);
-
-    //res.send(slicedArray)
-
-    //res.send(response)
 })
 
 export default router; 
